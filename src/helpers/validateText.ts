51c4251f7e156024e95b28a09e04cac5842b8cb9
Sebola3461 <mixvideostubecontato@gmail.com>
2023-10-28 15:44:54 -0300

    Fix word validation

--- src/helpers/validateText.ts
+++ src/helpers/validateText.ts
@@ -1,7 +1,26 @@
+import { Square } from "../components/Table/Square";
 import { GameSquareType } from "../providers/GameContext";
 
+function createPartialLettersValidationList(word: string) {
+  let validatedCharacters = {};
+
+  for (const letter of word) {
+    validatedCharacters[letter] = false;
+  }
+
+  return validatedCharacters as { [key: string]: boolean };
+}
+
+function findLetterOccurences(letter: string) {
+  const indices = [];
+
+  for (var i = 0; i < letter.length; i++) {
+    if (letter[i] == "s") indices.push(i);
+  }
+}
+
 export function validateText(word: string, input: string[]) {
-  const results: GameSquareType[] = [
+  const validationResult: GameSquareType[] = [
     GameSquareType.Clear,
     GameSquareType.Clear,
     GameSquareType.Clear,
@@ -9,112 +28,162 @@ export function validateText(word: string, input: string[]) {
     GameSquareType.Clear,
   ];
 
-  input.forEach((w, i) => {
-    w = w.trim();
+  console.log(word);
 
-    if (word[i] == w) results[i] = GameSquareType.Correct;
-  });
+  let partialLettersValidation = createPartialLettersValidationList(word);
 
-  const partialLetters: any = {};
+  for (let i = 0; i < 5; i++) {
+    // Validate correct
+    if (word[i] == input[i]) {
+      validationResult[i] = GameSquareType.Correct;
 
-  word.split("").forEach((w, i) => {
-    w = w.trim();
+      const occurrences = findLetterOccurences(input[i]);
 
-    if (partialLetters[w]) {
-      return (partialLetters[w] = {
-        s: partialLetters[w].s + 1,
-        p: partialLetters[w].p,
-      });
-    } else {
-      return (partialLetters[w] = {
-        s: 0,
-        p: 0,
+      validationResult.forEach((square, index) => {
+        if (validationResult[index] == GameSquareType.Partial)
+          validationResult[index] = GameSquareType.Filled;
       });
     }
-  });
 
-  const inputString = input.join();
-
-  input.forEach((letter, i) => {
-    letter = letter.trim();
-
-    let wordSize = getLetterCount(word)[letter] || 0;
-    let inputSize = getLetterCount(inputString)[letter] || 0;
-    const correctLength =
-      results.filter((r) => r == GameSquareType.Correct).length || 0;
-    let partialLettersValidation = word.split("").map((_letter) => {
-      return {
-        letter: _letter,
-        validated: false,
-      };
-    }) as { letter: string; validated: boolean }[];
-
-    console.log(
-      `Word: ${word} | Input Character: ${letter} | Has: ${wordSize} | Found: ${inputSize} | Correct: ${correctLength} | PartialSize: ${
-        inputSize - correctLength
-      }`
-    );
-
-    if (results[i] == GameSquareType.Correct) return;
+    // Validate partials
+    if (
+      word[i] != input[i] &&
+      word.includes(input[i]) &&
+      validationResult[i] != GameSquareType.Correct
+    ) {
+      partialLettersValidation[input[i]] = true;
+    }
 
     if (
-      word[i] != letter &&
-      word.includes(letter) &&
-      partialLetters[letter].p <= partialLetters[letter].s &&
-      results[i] != GameSquareType.Correct &&
-      !partialLettersValidation.find((l) => l.letter == letter).validated
+      partialLettersValidation[input[i]] == true &&
+      validationResult[i] != GameSquareType.Correct
     ) {
-      results[i] = GameSquareType.Partial;
-      partialLetters[letter].p = wordSize;
-      const partialLetterIndex = partialLettersValidation.findIndex(
-        (l) => l.letter == letter
-      );
-      partialLettersValidation[partialLetterIndex].validated = true;
+      const partialLetterIndex = input.indexOf(input[i]);
+
+      if (partialLetterIndex != -1)
+        validationResult[partialLetterIndex] = GameSquareType.Partial;
     }
-  });
+    // ==============
 
-  input.forEach((w, i) => {
-    if (results[i] == GameSquareType.Clear) results[i] = GameSquareType.Filled;
-  });
+    // Validate invalid
+    if (!word.includes(input[i])) {
+      validationResult[i] = GameSquareType.Filled;
+    }
 
-  return results;
-}
-
-function getLetterCount(word: string) {
-  let result = {};
-  for (let str of word) {
-    result[str] = result.hasOwnProperty(str) ? result[str] + 1 : 1;
+    if (validationResult[i] == GameSquareType.Clear) {
+      validationResult[i] = GameSquareType.Filled;
+    }
   }
 
-  return result;
-}
+  return validationResult;
 
-function getCorrectIndexes(results: GameSquareType[]) {
-  const r: number[] = [];
+  //   input.forEach((w, i) => {
+  //     w = w.trim();
 
-  results.forEach((s, i) => {
-    if (s == GameSquareType.Correct) r.push(i);
-  });
+  //     if (word[i] == w) results[i] = GameSquareType.Correct;
+  //   });
 
-  return r;
-}
+  //   const partialLetters: any = {};
 
-function getLetterIndex(string: string, character: string, cantbe: number[]) {
-  let r = -1;
+  //   word.split("").forEach((w, i) => {
+  //     w = w.trim();
 
-  string.split("").forEach((s, i) => {
-    if (s == character && !cantbe.includes(i) && r == -1) r = i;
-  });
+  //     if (partialLetters[w]) {
+  //       return (partialLetters[w] = {
+  //         s: partialLetters[w].s + 1,
+  //         p: partialLetters[w].p,
+  //       });
+  //     } else {
+  //       return (partialLetters[w] = {
+  //         s: 0,
+  //         p: 0,
+  //       });
+  //     }
+  //   });
 
-  return r;
-}
+  //   const inputString = input.join();
 
-function getValidatedIndexes(results: GameSquareType[]) {
-  let r: number[] = [];
+  //   input.forEach((letter, i) => {
+  //     letter = letter.trim();
 
-  results.forEach((s, i) => {
-    if (results[i] == GameSquareType.Correct) r.push(i);
-  });
+  //     let wordSize = getLetterCount(word)[letter] || 0;
+  //     let inputSize = getLetterCount(inputString)[letter] || 0;
+  //     const correctLength =
+  //       results.filter((r) => r == GameSquareType.Correct).length || 0;
+  //     let partialLettersValidation = word.split("").map((_letter) => {
+  //       return {
+  //         letter: _letter,
+  //         validated: false,
+  //       };
+  //     }) as { letter: string; validated: boolean }[];
 
-  return r;
+  //     console.log(
+  //       `Word: ${word} | Input Character: ${letter} | Has: ${wordSize} | Found: ${inputSize} | Correct: ${correctLength} | PartialSize: ${
+  //         inputSize - correctLength
+  //       }`
+  //     );
+
+  //     if (results[i] == GameSquareType.Correct) return;
+
+  //     if (
+  //       word[i] != letter &&
+  //       word.includes(letter) &&
+  //       partialLetters[letter].p <= partialLetters[letter].s &&
+  //       results[i] != GameSquareType.Correct &&
+  //       !partialLettersValidation.find((l) => l.letter == letter).validated
+  //     ) {
+  //       results[i] = GameSquareType.Partial;
+  //       partialLetters[letter].p = wordSize;
+  //       const partialLetterIndex = partialLettersValidation.findIndex(
+  //         (l) => l.letter == letter
+  //       );
+
+  //       partialLettersValidation[partialLetterIndex].validated = true;
+  //     }
+  //   });
+
+  //   input.forEach((w, i) => {
+  //     if (results[i] == GameSquareType.Clear) results[i] = GameSquareType.Filled;
+  //   });
+
+  //   return results;
+  // }
+
+  function getLetterCount(word: string) {
+    let result = {};
+    for (let str of word) {
+      result[str] = result.hasOwnProperty(str) ? result[str] + 1 : 1;
+    }
+
+    return result;
+  }
+
+  // function getCorrectIndexes(results: GameSquareType[]) {
+  //   const r: number[] = [];
+
+  //   results.forEach((s, i) => {
+  //     if (s == GameSquareType.Correct) r.push(i);
+  //   });
+
+  //   return r;
+  // }
+
+  // function getLetterIndex(string: string, character: string, cantbe: number[]) {
+  //   let r = -1;
+
+  //   string.split("").forEach((s, i) => {
+  //     if (s == character && !cantbe.includes(i) && r == -1) r = i;
+  //   });
+
+  //   return r;
+  // }
+
+  // function getValidatedIndexes(results: GameSquareType[]) {
+  //   let r: number[] = [];
+
+  //   results.forEach((s, i) => {
+  //     if (results[i] == GameSquareType.Correct) r.push(i);
+  //   });
+
+  //   return r;
 }
